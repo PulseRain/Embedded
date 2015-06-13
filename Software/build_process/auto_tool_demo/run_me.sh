0f48c9fe9d909ddd7df017f1d70272e89d365e47
PulseRain <pulserain@pulserain.com>
2015-06-12 20:05:46 -0700

    remove the unnecessary release folder

--- Software/build_process/auto_tool_demo/run_me.sh
+++ Software/build_process/auto_tool_demo/run_me.sh
@@ -7,12 +7,12 @@ rm -f demo
 rm -f demo.*
 
 aclocal
-cp -a /usr/share/automake-1.14/install-sh .
-cp -a /usr/share/automake-1.14/mkinstalldirs .
-cp -a /usr/share/automake-1.14/missing .
+cp -a /usr/share/automake-1.11/install-sh .
+cp -a /usr/share/automake-1.11/mkinstalldirs .
+cp -a /usr/share/automake-1.11/missing .
 touch NEWS AUTHORS ChangeLog
-cp -a /usr/share/automake-1.14/INSTALL .
-cp -a /usr/share/automake-1.14/COPYING .
+cp -a /usr/share/automake-1.11/INSTALL .
+cp -a /usr/share/automake-1.11/COPYING .
 
 automake --add-missing
 
@@ -23,17 +23,6 @@ automake
 echo " autoconf ..."
 echo "==========================================="
 autoconf
-
-echo " releasing ..."
-echo "==========================================="
-rm -rf ./release
-mkdir -p release
-cp -f ./configure ./release
-cp -f ./Makefile.am ./release
-cp -f ./Makefile.in ./release
-cp -f ./configure.ac ./release
-cp -a src ./release
-cd release;chmod 755 *
 
 echo " done"
 echo "==========================================="
